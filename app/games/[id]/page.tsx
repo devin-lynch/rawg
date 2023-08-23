46b389e25dce0659a11b2326a1f4a7e35287f4b3
devin-lynch <devin.l.lynch@gmail.com>
2023-08-23 15:13:29 -0500

    Display individual game info

--- app/games/[id]/page.tsx
+++ app/games/[id]/page.tsx
@@ -31,10 +31,23 @@ export default function Page({ params }: { params: { id: number } }) {
     buildGame();
   }, []);
 
-  return (
-    <div>
-      <p>{`${params.id}`}</p>
-      {game ? <p>{game.id}</p> : null}
+  const displayGameInfo = game ? (
+    <div className="text-center">
+      <div>
+        <img src={game.background_image} alt={game.name} />
+      </div>
+      <p className="text-4xl">{game.name}</p>
+      <p className="text-xl">Rated {game.esrb_rating.name}</p>
+      <p className="text-xl">{game.released}</p>
+      <p className="text-xl">Metacritic: {game.metacritic}</p>
+      <p className="text-xl">
+        <a href={game.website}>Website</a>
+      </p>
+      <p className="text-xl">
+        <a href={game.reddit_url}>Subreddit</a>
+      </p>
     </div>
-  );
+  ) : null;
+
+  return <div>{displayGameInfo}</div>;
 }
